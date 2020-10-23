ee08362e30d56f69dacdf71b1d3b8157c3a8b7d6
Rajani Raju <rajaniraju2003@gmail.com>
2020-10-23 08:59:00 -0500

    Now can click only once. Corrected in child's state. flagged in the state.

--- src/Square.tsx
+++ src/Square.tsx
@@ -8,16 +8,30 @@ type Props = {
   col: number;
   symbol: any;
 };
+type State = {
+  touch: Boolean;
+};
 
-class Square extends React.Component<Props> {
+class Square extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      touch: false,
+    }
+  }
   onPlayerClick = () => {
+    if (this.state.touch) {
+      return
+    }
     console.log(this.props.player);
     console.log(this.props.row);
     console.log(this.props.col);
     //if statement 26 is true cannot enter the button click.
-
+   
     this.props.onPlayerChange(this.props.row, this.props.col);
-
+    this.setState({
+      touch: true,
+    })
     console.log(this.props.player);
   };
 
